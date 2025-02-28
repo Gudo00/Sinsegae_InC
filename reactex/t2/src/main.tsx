b46adfb44cabed592cc99ab6dba48322f79252a9
gudo <eveiseve12@gmail.com>
2025-02-28 16:12:36 +0900

    TypeScript kiosk, todo

--- reactex/t2/src/main.tsx
+++ reactex/t2/src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+//npm install -D tailwindcss postcss autoprefixer
+//npx tailwind init -p
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
