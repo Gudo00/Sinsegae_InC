096f03ae4c264872dd0d1529a0e905a121c4ca91
gudo <eveiseve12@gmail.com>
2025-03-18 15:20:26 +0900

    mod

---
+++ reactex/t6/src/pages/todo/addPage.tsx
@@ -0,0 +1,57 @@
+import {useNavigate} from "react-router";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {postTodo} from "../../api/todoApi.tsx";
+
+
+const sampleState:TodoAdd = {
+    title:'Test 🎷',
+    writer:'tester',
+    dueDate:'2025-03-14'
+}
+
+
+function AddPage() {
+
+    const navigate = useNavigate()
+
+    const addMutation = useMutation({
+        mutationFn: (todo:TodoAdd) => postTodo(todo),
+        onSuccess: data =>  {
+            console.log("onSuccess" , data)
+        }
+    })
+
+    const handleClick = () => {
+
+        addMutation.mutate(sampleState)
+
+    }
+
+    const queryClient = useQueryClient()
+
+    const moveToList = () => {
+
+        queryClient.invalidateQueries({queryKey:['todo/list'] })
+        //console.log(addMutation.data)
+        navigate('/todo/list')
+    }
+
+
+    return (
+        <div>
+
+            {addMutation.isPending && <h1>처리중입니다..........</h1>}
+
+            {addMutation.isSuccess &&
+                <h1 onClick={moveToList} >
+                    처리완료 {addMutation.data.TNO}
+                </h1>
+            }
+
+            <div>Todo Add</div>
+            <button onClick={handleClick}>ADD</button>
+        </div>
+    );
+}
+
+export default AddPage;
