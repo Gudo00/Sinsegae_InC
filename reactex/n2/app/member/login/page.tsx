d4f9ce9441f55fb7fa1842b0d72f7613d1f5bab3
Gudo00 <eveiseve12@gmail.com>
2025-04-28 00:39:20 +0900

    next 2nd

--- reactex/n2/app/member/login/page.tsx
+++ reactex/n2/app/member/login/page.tsx
@@ -3,8 +3,10 @@
 
 
 
-import {useActionState} from "react";
+import {useActionState, useEffect} from "react";
 import {loginServerAction} from "@/app/member/memberActions";
+import Link from "next/link";
+import {useLoginStore} from "@/store/useLoginStore";
 
 const initState: MemberInfo = {
     mid:'',
@@ -26,12 +28,39 @@ function LoginPage() {
 
     const [state,action, isPending] = useActionState(loginAction, initState);
 
+    const {save} = useLoginStore()
+
+    useEffect(()=>{
+
+        console.log("access Token changed............" + state.accessToken)
+
+        if(state.accessToken) {
+
+            save(state.mid, state.accessToken, state.refreshToken)
+
+        }
+
+
+    },[state.accessToken])
+
+
     return (
         <div>
+
+            {isPending && <div className={'bg-blue-600'}>로그인 중.................</div>}
+
+            {state.accessToken && <div className={'bg-amber-600'}>로그인 완료되었음</div>}
+            
             <div>Login Page</div>
             <form action={action}>
                 <button type={"submit"}>Login</button>
             </form>
+
+            <div>
+
+                <Link href={'/member/mypage'}>MyPage</Link>
+            </div>
+
         </div>
     )
 }
--- springex/sb2/src/main/java/org/zerock/sb2/board/repository/BoardRepository.java
+++ springex/sb2/src/main/java/org/zerock/sb2/board/repository/BoardRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.repository.query.Param;
 import org.zerock.sb2.board.dto.BoardReadDTO;
 import org.zerock.sb2.board.entities.BoardEntity;
 
-public interface BoardRepository extends JpaRepository<BoardEntity, Long>, BoardSearch{
+public interface BoardRepository extends JpaRepository<BoardEntity, Long>, BoardSearch {
 
-//bno, title, content, writer, delFlag, viewCnt, regDate, modDate
+  //bno, title, content, writer, delFlag, viewCnt, regDate, modDate
   @Query("select new org.zerock.sb2.board.dto.BoardReadDTO(b.bno, b.title, b.content, b.writer, b.delFlag, b.viewCnt, b.regDate, b.modDate)  " +
-  " from BoardEntity b where b.bno = :bno")
-  BoardReadDTO selectOne( @Param("bno") Long bno);
-  
+          " from BoardEntity b where b.bno = :bno")
+  BoardReadDTO selectOne(@Param("bno") Long bno);
+
 }
